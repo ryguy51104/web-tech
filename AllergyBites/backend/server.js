d1cf2b3817b5cfcb52675739ef729344ddd44e92
snacj <bbmelvin@gmx.de>
2025-03-06 15:21:02 +0100

    Change password API endpoint created.

--- AllergyBites/backend/server.js
+++ AllergyBites/backend/server.js
@@ -3,6 +3,8 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import User from "./models/user.model.js";
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 // loading environment variables
 dotenv.config();
@@ -38,6 +40,56 @@ app.post('/api/users', async (req, res) => {
         console.error("Error while creating user", error);
         res.status(500).json({ success: false, message: "Server error" });
     }
+});
+
+// change password
+app.put('/api/users/:id', async (req, res) => {
+
+    // get id from request parameters
+    const { id } = req.params;
+
+    // get password from request body
+    const { password } = req.body;
+    
+    // check if id is valid
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Invalid user id" });
+    }
+
+    // change password
+    try {
+        // find user by id
+        const user = await User.findById(id);
+        if(!user) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        } 
+
+        // updating and hashing the password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.status(200).json({ success: true, data: user, message: "Password updated successfully" });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+});
+
+// route to delete users
+app.delete('/api/users/:id', async (req, res) => {
+
+    // get id from parameters
+    const { id } = req.params;
+
+    // try deleting user with id
+    try {
+        await User.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "User deleted successfully" });
+    } catch (error) {
+        res.status(404).json({ success: false, message: "User not found" });
+    }
+
 });
 
 
