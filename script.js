3b79d65db25307d5d36f6505ebe722a28b373aad
Maximus <62805287+PapyRGB@users.noreply.github.com>
2025-03-10 20:30:40 +0000

    clearing main for branch merging
    main was full of test stuff, and it's easier to clear it and keep this stuff in a backup branc.

--- script.js
+++
@@ -1,9 +0,0 @@
-console.log("hello cruel world");
-
-function showMessage() {
-    console.log("hello cruel world");
-    alert("Hello from JavaScript!");
-}
-
-// Call the function to execute it
-showMessage();
